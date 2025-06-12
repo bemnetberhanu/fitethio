b4734a7272d7c5353957df4793bd4f9ed9f34989
bemnetberhanu <bemnetberhanu98@gmail.com>
2025-06-12 15:55:51 -0300

    1

---
+++ app/src/main/java/com/example/fitethio/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.fitethio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.fitethio.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        binding.startbtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MainActivity2.class)));
+
+
+
+
+    }
+}
